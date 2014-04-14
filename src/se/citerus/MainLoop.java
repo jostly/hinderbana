e85ca6ca67e6a16df7a96ffcbc07b06fe331571b
Johan Östling <johan.f.ostling@gmail.com>
2014-04-14 18:31:53 +0200

    Initial commit

---
+++ src/se/citerus/MainLoop.java
@@ -0,0 +1,45 @@
+package se.citerus;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MainLoop extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        primaryStage.setScene(scene);
+
+        Circle circle = new Circle(150, Color.web("white", 0.5));
+        circle.setStrokeType(StrokeType.OUTSIDE);
+        circle.setStroke(Color.web("white", 0.9));
+        circle.setStrokeWidth(4);
+
+        root.getChildren().add(circle);
+
+        Timeline gameLoop = new Timeline(new KeyFrame(Duration.millis(10), event -> {
+            System.out.println("TICK!");
+        }));
+        gameLoop.setCycleCount(Animation.INDEFINITE);
+
+        gameLoop.play();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
